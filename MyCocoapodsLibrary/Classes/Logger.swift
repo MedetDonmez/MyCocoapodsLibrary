1e8321b2b6f5b2a639f65a770aba530033ea8a05
Medet Dönmez <hiimmdt@gmail.com>
2022-12-06 10:43:39 +0300

    first commit

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by fmss on 6.12.2022.
+//
+
+import Foundation
+
+public class Logger {
+    
+    public init(){}
+    
+    public func printLog() {
+        print("Hello world")
+    }
+    
+    public func privateMethod() {
+        print("private")
+    }
+}
